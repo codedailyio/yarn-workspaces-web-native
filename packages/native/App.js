cfc784a2ad68914e7cdc986a4ccf20b24065a83f
Jason Brown <browniefed@gmail.com>
2018-02-27 20:25:40 -0800

    Add final working code

--- packages/native/App.js
+++ packages/native/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SharedInfo from "shared";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{SharedInfo}</Text>
       </View>
     );
   }
